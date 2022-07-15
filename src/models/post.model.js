aa492d504ccf9c2e7a767cd2165f4dbf140b12f1
ErnestoGarSa <ernest0_10@hotmail.com>
2022-07-14 21:02:56 -0500

    ADD post model

---
+++ src/models/post.model.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose")
+
+const postSchema = new mongoose.Schema({
+    photo:{
+        type: String
+    },
+    userName:{
+        type: String,
+        required:true,
+        minlength:3,
+        maxlength:15
+    },
+    profilePic:{
+        type: String
+    },
+    title:{
+        type: String,
+        required:true,
+        minlength:8,
+        maxlength:25
+    },
+    hashTag:{
+        type: String,
+        minlength:2,
+        maxlength:10
+    },
+    post:{
+        type: String,
+        required:true,
+        minlength:3,
+    },
+    readTime:{
+        type: String,
+        required:true,
+    },
+    date:{
+        type: String,
+        required:true,
+    } 
+})
+
+module.exports=mongoose.model("posts", postSchema)
